fbca845bc35d4a503136d7888b6b221b9e9c88a1
Arun Kumar Gupta <arunkrguptaa@gmail.com>
2020-01-11 02:00:24 +0530

    first method

--- stoge.js
+++ stoge.js
@@ -1,1 +1,14 @@
-console.log('stoge js')
+(function(){
+
+    var arrProto = Array.prototype;
+    var coreJoin = arrProto.join
+
+    //Array Methods
+
+    // ex stoge.join([1,2,3], '|') => '1|2|3'
+    function join(arr, liner) {
+        return arr == null ? '' : coreJoin.call(arr, liner);
+    }
+   
+
+}.call(this))
