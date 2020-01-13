c73e7a1f529cb627909d2e9d4725ae237813989e
Arun Kumar Gupta <arunkrguptaa@gmail.com>
2020-01-13 21:06:21 +0530

    new method added

--- stoge.js
+++ stoge.js
@@ -1,6 +1,10 @@
 module.exports = (function(){
+    // Native variables
     var arrProto = Array.prototype;
     var coreJoin = arrProto.join;
+
+    // Method specific variables
+    var mailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,6}$/;
 
     //Array Methods
 
@@ -9,9 +13,45 @@ module.exports = (function(){
         return arr == null ? '' : coreJoin.call(arr, liner);
     }
 
+    //String Methods
+
+    // ex 'abc@abc.org' => true
+    function isMail(str) {
+        return (typeof str == 'string') ? mailRegex.test(str) : false
+    }
+
+    // ex - lower => l o w e r
+    function distinct(str){
+        return (typeof str !== 'string') 
+        ? str.toString()
+        : str.split('').join(' ').trim();
+    }
+
+    // ex xyz@$#(),;!$%^&*+-_.+=abc => xyzabc
+    function mediocre(str){
+        return (typeof str !== 'string') 
+        ? str.toString()
+        : str.replace(/[^a-zA-Z0-9 ]/g,'');
+    }
+
+    // ex xyz@$#(),;!$%^&*+-_.+=abc => @$#(),;!$%^&*+-_.=+
+    function special(str){
+        return (typeof str !== 'string') 
+        ? str.toString()
+        : str.replace(/[a-zA-Z0-9 ]/g,'');
+    }
+    
     // namespace for stoge methods
     var stoge = {
-        join
+        //Array methods namespace
+        join,
+
+        //String methods namespace
+        isMail,
+        distinct,
+        mediocre,
+        special,
+        
     };
     return stoge;
 
