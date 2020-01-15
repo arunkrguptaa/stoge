58d48345cebe9cfa83b02c5f0ec7b71505589a97
Arun Kumar Gupta <arunkrguptaa@gmail.com>
2020-01-16 02:29:24 +0530

    new number methods added

--- stoge.js
+++ stoge.js
@@ -1,7 +1,7 @@
 module.exports = (function(){
     // Native variables
-    var arrProto = Array.prototype;
-    var coreJoin = arrProto.join;
+    var arrProto = Array.prototype,
+    coreJoin = arrProto.join;
 
     // Method specific variables 
     var mailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,6}$/;
@@ -11,15 +11,15 @@ module.exports = (function(){
         '>': '&gt;',
         '"': '&quot;',
         "'": '&#39;'
-    }
-    var htmlSymbol = {
+    },
+    htmlSymbol = {
         '&amp;': '&',
         '&lt;': '<',
         '&gt;': '>',
         '&quot;': '"',
         '&#39;': "'"
-    }
-    var htmlescapeRegex = /[&<>"']/g;
+    },
+    htmlescapeRegex = /[&<>"']/g;
     var unescapeHtml = /&(?:amp|lt|gt|quot|#39);/g
 
     //Array Methods
@@ -33,14 +33,14 @@ module.exports = (function(){
 
     // ex range(2,1,5) => true
     function range(num, low, upp){
-        if(low == undefined && upp == undefined){
+        if(low === undefined && upp === undefined){
             return true;
         }
-        if(upp == undefined){
+        if(upp === undefined){
             upp = low;
             low = 0;
         }
-        if(typeof num == 'number' && typeof low == 'number' && typeof upp == 'number'){
+        if(typeof num === 'number' && typeof low === 'number' && typeof upp === 'number'){
             return (num > low && num < upp) ? true : false
         }else {
             return false
@@ -49,17 +49,34 @@ module.exports = (function(){
     
     // ex random(1,3) => 3
     function random(s, e){
-        return (typeof s == 'number' && typeof e == 'number')
+        return (typeof s === 'number' && typeof e === 'number')
         ? Math.floor(Math.random() * (e-s+1) + s)
         : (num || '');
     }
+
+    // ex 140 => 60
+    function fToCelsius(f, fixed){
+        return (typeof f === 'number')
+        ? (fixed && typeof fixed === 'number') 
+            ? parseFloat((5*((f-32)/9)).toFixed(fixed))
+            : (5*((f-32)/9))
+        : (f || '');
+    }
     
+    // ex 60 => 140
+    function cToFahrenheit(c, fixed){
+        return (typeof c === 'number')
+        ? (fixed && typeof c === 'number') 
+            ? parseFloat(((9*(c/5))+32).toFixed(fixed))
+            : ((9*(c/5))+32)
+        : (c || '');
+    }
 
     //String Methods
 
     // ex 'abc@abc.org' => true
     function isMail(str,pattern) {
-        return (typeof str == 'string') 
+        return (typeof str === 'string') 
         ? (pattern) 
             ? pattern.test(str) 
             : mailRegex.test(str) 
@@ -68,69 +85,69 @@ module.exports = (function(){
 
     // ex - lower => l o w e r
     function distinct(str){
-        return (typeof str == 'string') 
+        return (typeof str === 'string') 
         ? str.split('').join(' ').trim()
         : (str || '');
     }
 
     // ex xyz@$#(),;!$%^&*+-_.+=abc => xyzabc
     function mediocre(str){
-        return (typeof str == 'string') 
+        return (typeof str === 'string') 
         ? str.replace(/[^a-zA-Z0-9 ]/g,'')
         : (str || '');
     }
 
     // ex xyz@$#(),;!$%^&*+-_.+=abc => @$#(),;!$%^&*+-_.=+
     function specialChar(str){
-        return (typeof str == 'string') 
+        return (typeof str === 'string') 
         ? str.replace(/[a-zA-Z0-9 ]/g,'')
         : (str || '');
     }
 
     // ex BLUNT => Blunt
     function capitalize(str){
-        return (typeof str == 'string')
+        return (typeof str === 'string')
         ? str.charAt(0).toUpperCase()+str.slice(1).toLowerCase()
         : (str || '');
     }
 
     // ex 'abc def ghi' => abcDefGhi
     function camel(str){
-        return (typeof str == 'string')
+        return (typeof str === 'string')
         ? str.trim().split(' ').map((e,i) => (i!==0) ? capitalize(e) : e).join('')
         : (str || '');
     }
 
     // ex 'abc def ghi' => abc_def_ghi
     function snake(str){
-        return (typeof str == 'string')
+        return (typeof str === 'string')
         ? str.trim().split(' ').join('_')
         : (str || '');
     }
 
     // ex 'abc def ghi' => abc_def_ghi
     function kebab(str){
-        return (typeof str == 'string')
+        return (typeof str === 'string')
         ? str.trim().split(' ').join('-')
         : (str || '');
     }
 
     // ex 'abc def ghi' => abc_def_ghi
     function pascal(str){
-        return (typeof str == 'string')
+        return (typeof str === 'string')
         ? str.trim().split(' ').map((e,i) => capitalize(e)).join('')
         : (str || '');
     }
 
     // ex 'abc&><def' => 'abc&amp;&gt;&lt;def'
     function escape(str){
-        return (typeof str == 'string')
+        return (typeof str === 'string')
         ? str.replace(htmlescapeRegex, (chr) => htmlEntities[chr])
         : (str || '');
     }
     // ex 'abc&amp;&gt;&lt;def' => 'abc&><def'
     function unescape(str, htmlescapeRegex){
-        return (typeof str == 'string')
+        return (typeof str === 'string')
         ? str.replace(unescapeHtml, (chr) => htmlSymbol[chr])
         : (str || '');
     }
@@ -143,6 +160,8 @@ module.exports = (function(){
         //Number methods namespace
         random,
         range,
+        fToCelsius,
+        cToFahrenheit,       
 
         //String methods namespace
         isMail,
