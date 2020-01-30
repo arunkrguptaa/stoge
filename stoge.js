2e1f90e08227552c800d7e898c19360347efbedc
Arun Kumar Gupta <arunkrguptaa@gmail.com>
2020-01-30 12:07:30 +0530

    binary methods changes

--- stoge.js
+++ stoge.js
@@ -57,12 +57,10 @@ module.exports = (function(){
             var first  = 0,
             last   = arr.length - 1,
             middle = Math.floor((last + first)/2);
-            while(arr[middle] != val && first < last)
-            {
-            if (val < arr[middle]){
+            while(arr[middle] != val && first < last){
+                if(val < arr[middle]){
                     last = middle - 1;
-                } 
-            else if (val > arr[middle]){
+                } else if(val > arr[middle]){
                     first = middle + 1;
                 }
                 middle = Math.floor((last + first)/2);
