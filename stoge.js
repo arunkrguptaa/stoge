e42f03a39aad564e126d7441706c85e59d861caa
Arun Kumar Gupta <arunkrguptaa@gmail.com>
2020-02-03 01:37:02 +0530

    version update

--- stoge.js
+++ stoge.js
@@ -2,7 +2,7 @@ module.exports = (function(){
     // Native variables
 
     // Method specific variables 
-    var version = '1.1.1';
+    var version = '1.3.1';
     var mailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,6}$/;
     var htmlEntities = {
         '&': '&amp;',
